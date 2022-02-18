e88c13a327664a8c602e0d51ba42709ec78bf0e4
Giacomo <giacomo.flaim.91@gmail.com>
2022-02-18 11:29:11 +0100

    scroll

--- js/js-slider.js
+++ js/js-slider.js
@@ -1,6 +1,19 @@
+
+$("#primo").click(function() {
+  var elmnt = document.getElementById("miodiv");
+  elmnt.scrollIntoView({behavior: "smooth"});
+});
+
+
 const scrollContainer = document.querySelector("main");
 
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
 });
+
+
+
+    
+
+    
